b7311e754e164269299e8ddffd9ec487c18ff6b0
visual studio <vs@itstep.ge>
2023-12-16 19:53:59 +0400

    Start

---
+++ DroneMarket/DroneMarket.API/Models/DroneModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DroneMarket.API.Models
+{
+    public class DroneModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int MaxFlightRadius { get; set; }
+
+        [Required]
+        public bool HasWifiOrBluetooth { get; set; }
+    }
+}
